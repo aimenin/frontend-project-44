feae54c7bb69734c9614799e0709919638945616
aimen <artem.igorevich8@rambler.ru>
2023-11-07 19:52:02 +0300

    перенесет brain-even в правильную директорию

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,39 +1,5 @@
 #!/usr/bin/env node
-import {
-  BOTTOM_RANDOM_BORDER,
-  TOP_RANDOM_BORDER,
-  answers, askQuestion, constructQuestions, game, getRandomNumber,
-} from '../src/index.js';
-
-const isEven = (number) => number % 2 === 0;
-
-const ask = (question) => {
-  const answer = askQuestion(question);
-
-  if (
-    (isEven(question) && answer === 'yes') || (!isEven(question) && answer === 'no')
-  ) {
-    console.log('Correct!');
-    return true;
-  }
-
-  console.log(
-    `'${answer}' is wrong answer ;(. Correct answer was '${answers[Number(answers[0] === answer)]}'.`,
-  );
-  return false;
-};
-
-const constructQuestion = () => getRandomNumber(BOTTOM_RANDOM_BORDER, TOP_RANDOM_BORDER);
-
-const evenGame = () => {
-  console.log('Answer "yes" if the number is even, otherwise answer "no".');
-  const questions = constructQuestions(constructQuestion);
-  for (let i = 0; i < questions.length; i += 1) {
-    if (!ask(questions[i])) {
-      return false;
-    }
-  }
-  return true;
-};
+import { game } from '../src/index.js';
+import evenGame from '../src/games/brain-even.js';
 
 game(evenGame);
--- src/index.js
+++ src/index.js
@@ -25,6 +25,12 @@ export const answers = ['yes', 'no'];
 
 export const askYesNoQuesion = (question) => answers[readlineSync.keyInSelect(answers, `Question: ${question}`)];
 
+export const showCorrect = () => console.log('Correct!');
+
+export const showWrongAnswer = (answer, rightAnswer) => `'${answer}' is wrong answer ;(. Correct answer was '${rightAnswer}'.`;
+
+export const showQuestion = (question) => console.log(question);
+
 export const game = (gameFunction) => {
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
